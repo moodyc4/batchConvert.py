b49e9201115a6b56ad4e8d78c7edfb3f6fe5f90e
Caleb <cal.moody31@gmail.com>
2023-01-19 12:24:51 -0600

    Added python file
    Needs to be ironed out

---
+++ convert.py
@@ -0,0 +1,45 @@
+﻿import os
+
+
+def convert(prefix, path, outName):
+    i = 1
+    for filename in os.listdir(path):
+        if (filename.endswith(".mp4")):
+            if (outName == ""):
+                x = filename.split('.')
+                newName = prefix + x[0] + "_" + str(i) + ".mp4"
+            else:
+                newName = prefix + outName + "_" + str(i) + ".mp4"
+            try:
+                os.system("ffmpeg -i '{0}' {1}".format(filename, newName))
+                i = i + 1
+            except:
+                print("ffmpeg bro chunked!")
+        else:
+            continue
+    print("Done")
+
+
+path = input("Path to convert: ")
+
+if (path == ""):
+    path = os.getcwd()
+elif (path == "1"):
+    path = "/Users/cmoody/Desktop/20230113"
+try:
+    os.chdir(path)
+except:
+    print("Bad Path?")
+    path = ""
+
+outputDir = path + "/" + "Converted"
+outName = input("Name for output files: ")
+#if not (os.path.isdir(outputDir)):
+try:
+    os.mkdir(outputDir)
+except:
+    print("Folder exists... Continuing...")
+prefix = outputDir + "/"
+convert(prefix, path, outName)
+#print("Couldn't create output folder")
+    
