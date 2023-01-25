45d4fe29d664200a234cb383e67bfb8d94eea58b
Caleb <cal.moody31@gmail.com>
2023-01-24 19:16:36 -0600

    Working terminal script

--- convert.py
+++ convert.py
@@ -1,16 +1,33 @@
-﻿#!/usr/bin/env python
+﻿#!/usr/bin/python3
 import os
 import sys
 
 #List of current commands:
-h = ["-h", "\n--------------------\nUsage: \nconvert.py [Path to folder containing files] [Name for new files]\n\nNew Files will be saved in a folder named 'Converted'\n--------------------\n"]
-c = ["-c [path]", "converts files in given dir. If none specified, current working dir will be used."]
+c = ["'-c' [Directory] [Name] | Converts files in given directory. If not specified, current dir will be used.", "'-h' | Shows help"]
+h = "\n--------------------\nBatch Converter Usage\nCommands:\n\n{0}\n\n{1}\n\nNew Files will be saved in a folder named 'Converted'\n--------------------\n".format(c[0], c[1])
 
 def help():
-    print(h[1])
+    print(h)
 
-def convert(prefix, path, outName):
+def convert(*args):
     i = 1
+    outName = ""
+    if (len(args) == 1):
+        path = args[0]
+    elif (len(args) == 2):
+        path = args[0]
+        outName = args[1]
+    try:
+        os.chdir(path)
+    except:
+        print("Bad Path?")
+        path = ""
+    outputDir = path + "/" + "Converted"
+    try:
+        os.mkdir(outputDir)
+    except:
+        print("Folder exists... Continuing...")
+    prefix = outputDir + "/"
     for filename in os.listdir(path):
         if (filename.endswith(".mp4")):
             if (outName == ""):
@@ -20,52 +37,30 @@ def convert(prefix, path, outName):
                 newName = prefix + outName + "_" + str(i) + ".mp4"
             try:
                 os.system("ffmpeg -i '{0}' '{1}'".format(filename, newName))
-                i = i + 1
             except:
                 print("ffmpeg bro chunked!")
         else:
             continue
+        i = i + 1
     print("Done")
 
 def getInput(args):
-    i = 0
-    prefix = ""
     path = ""
-    outName = ""
+    if (len(args) > 5):
+        print("Too many arguments. '-h' for help.")
+    elif (len(args) < 3):
+        a = input("No folder or output name specified, default location and names will be used. \nContinue? [Y/N] ")
+        if (a.lower() == "y" or a.lower() == ""):
+            path = os.getcwd()
+            convert(path)
+    else:
+        path = args[2]
+        if (len(args) > 3):
+            outName = args[3]
+            convert(path, outName)
+        else:
+            convert(path)
 
-    for arg in args:
-        if (len(args) > 5):
-            print("Too many arguments. '-h' for help.")
-            break
-        elif (len(args) < 3):
-            a = input("No folder or output name specified, default location and names will be used. \nContinue? [Y/N] ")
-            if (a.lower() == "y" or a.lower() == ""):
-                convert()
-            else:
-                break
-        elif (i == 2):
-            print(arg)
-        elif (i == 3):
-            print(arg)
-        elif (i == 4):
-            print(arg)
-        i = i + 1
-
-    if (path == ""):
-        path = os.getcwd()
-    try:
-        os.chdir(path)
-    except:
-        print("Bad Path?")
-        path = ""
-
-    outputDir = path + "/" + "Converted"
-    try:
-        os.mkdir(outputDir)
-    except:
-        print("Folder exists... Continuing...")
-    prefix = outputDir + "/"
-    convert(prefix, path, outName)
 
 def main():
     if (len(sys.argv) == 1):
